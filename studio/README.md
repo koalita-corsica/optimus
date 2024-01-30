e60f050d952a65f88ef930a71304dc699029d8cf
koalita-corsica <jiann@koalita.corsica>
2024-01-30 10:00:16 +0100

    Ajout favicon dans le ReadMe + ajout locale-Fr/FR dans les plugins pour la traduction du studio

--- studio/README.md
+++ studio/README.md
@@ -5,4 +5,6 @@
   - Modifier [sanity.config.ts] avec les bons dataset
   - Modifier [sanity.cli.ts] avec les bons dataset
   - yarn puis yarn upgrade-interactive pour tout mettre à jour
+  - Mettre les différentes version du favicon dans le dossier static
+  - Mettre le logo dans le dossier static et mettre à jour sanity.config.ts
   - Trier les documents et les objets :p
--- studio/sanity.config.ts
+++ studio/sanity.config.ts
@@ -2,6 +2,11 @@ import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
+import {frFRLocale} from '@sanity/locale-fr-fr'
+
+// const Logo = () => (
+//   <img src="/static/[LOGO]" style={{objectFit:'cover', height:'100%', width: '100%'}} />
+// );
 
 export default defineConfig({
   name: 'default',
@@ -10,7 +15,9 @@ export default defineConfig({
   projectId: 'ftbtijdq',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool()],
+  // icon: Logo,
+
+  plugins: [deskTool(), visionTool(), frFRLocale()],
 
   schema: {
     types: schemaTypes,

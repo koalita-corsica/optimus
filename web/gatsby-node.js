841714a0e35af16719b199380851da8d080f604b
koalita-corsica <jiann@koalita.corsica>
2024-01-30 09:35:10 +0100

    Maj pour revert les commits vacelli

--- web/gatsby-node.js
+++ web/gatsby-node.js
@@ -16,3 +16,61 @@ exports.createPages = async ({ actions }) => {
     defer: true,
   })
 }
+// Ci dessous un exemple fonctionnel
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions;  
+
+//   const article = graphql(`
+//   query Article {
+//       articles: allSanityBlog {
+//         nodes {
+//           _id
+//           slug {
+//               current
+//           }
+//         }
+//       }
+//     }
+// `).then(result => {
+//   result.data.articles.nodes.forEach((item) => {
+//     const slug = "blog/" + item.slug.current
+//     const id = item._id
+//     createPage({
+//       path: slug,
+//       component: require.resolve(`./src/templates/article.js`),
+//       context: { id: id },
+//     })
+//   })
+// })
+
+
+// const client = graphql(`
+//   query Client {
+//       clients: allSanityPartenaires {
+//         nodes {
+//           _id
+//           name
+//         }
+//       }
+//     }
+// `).then(result => {
+//   result.data.clients.nodes.forEach((item) => {
+//     const url = item.name.normalize("NFD")
+//     .replace(/[\u0300-\u036f]/g, '')
+//     .replace(/[^a-zA-Z0-9]/g, ' ')
+//     .replace(/\s+/g, '-')
+//     .replace(/-+$/, '')
+//     .slice(0, 200)
+//     .toLowerCase()
+//     const slug = "realisations/" + url
+//     const id = item._id
+//     createPage({
+//       path: slug,
+//       component: require.resolve(`./src/templates/client.js`),
+//       context: { id: id },
+//     })
+//   })
+// })
+
+//   return Promise.all([article, client])
+// }
